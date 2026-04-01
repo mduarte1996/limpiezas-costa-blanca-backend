081db314a2225e0671ad88181145e253669c8816
maria fernanda duarte <mfda.1396@gmail.com>
2026-04-01 23:19:48 +0200

    fix: import User and Review

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from extensions import db, migrate
-from models import ServiceRequest
+from models import ServiceRequest, Review, User
 from datetime import datetime
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, create_access_token
